6d96b68e93370058c9f467cc4725354f7ef886e5
Zen-cronic <83657429+Zen-cronic@users.noreply.github.com>
2024-05-30 18:42:41 -0400

    continue: recursive dir

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ const fs = require("fs");
 const cp = require("child_process");
 
 const { customFetch } = require("./customFetch");
-const { transformUrlStr } = require("./utils");
+const {
+  transformUrlStr,
+  ensureDirExists,
+  checkOpts,
+  generateCodeFile,
+} = require("./utils");
 
 const CODE_DIR_PATH = path.join(__dirname, "code");
 const WORD_DIR_PATH = path.join(__dirname, "word");
@@ -14,31 +19,73 @@ module.exports = {
   WORD_DIR_PATH,
 };
 
+/**
+ * @typedef {Object} DefaultOpts
+ * @property {boolean} [singleFile] - If true, only a single file will be processed.
+ * @property {boolean} [dir] - If true, files in the directory will be processed.
+ * @property {boolean} [recursive] - If true, the files will be processed recursively.
+ */
 
 /**
  *
  * @param {string | URL} url
- * @param {Object} opts
+ * @param {DefaultOpts} opts
+ */
+
+// const defaultOpts = {
+//   singleFile: true,
+//   dir: false,
+//   recursive: false,
+// };
+/**
+ *
+ * @param {string | URL} url
+ * @param {DefaultOpts} opts
  */
 async function printLib(url, opts) {
   //for cli eth is string
 
-  url = transformUrlStr(url);
+  try {
+    opts = checkOpts(opts);
 
-  const res = await customFetch(url);
-  const files = res.data;
+    url = transformUrlStr(url);
 
-  const downloadUrls = [];
-  for (const f of files) {
-    // downloadUrls.push({ [f.name]: f.download_url });
-    downloadUrls.push({ name: f.name, download_url: f.download_url });
-  }
+    ensureDirExists(WORD_DIR_PATH);
+    ensureDirExists(CODE_DIR_PATH);
 
-  if (!fs.existsSync(CODE_DIR_PATH)) {
-    fs.mkdirSync(CODE_DIR_PATH);
-  }
-  if (!fs.existsSync(WORD_DIR_PATH)) {
-    fs.mkdirSync(WORD_DIR_PATH);
+    if (opts.singleFile) {
+      const res = await customFetch(url);
+      const { content, encoding, name } = res.data;
+
+      //base64
+      const encodedBuf = Buffer.from(content, encoding);
+
+      const decodedContent = encodedBuf.toString("utf-8");
+
+      generateCodeFile(CODE_DIR_PATH, name, decodedContent);
+
+    } else if (opts.dir && !opts.recursive) {
+      const res = await customFetch(url);
+
+      const files = res.data.filter(
+        (f) => f.size != 0 && f.download_url && f.type == "size"
+      );
+
+      //files.foreach - does NOT wait
+      for (const f of files) {
+        const textContent = await customFetch(f.download_url);
+        generateCodeFile(CODE_DIR_PATH, f.name, textContent);
+      }
+
+      //recursive
+    } else if (opts.dir && opts.recursive) {
+
+      url = transformUrlStr(url)
+
+
+    }
+  } catch (error) {
+    throw error;
   }
 
   for (const o of downloadUrls) {
@@ -75,5 +122,3 @@ async function printLib(url, opts) {
     throw error;
   }
 }
-
-
--- src/utils.js
+++ src/utils.js
@@ -1,36 +1,102 @@
+const fs = require("fs");
+const path = require("path");
 
+module.exports = {
+  transformUrlStr,
+  ensureDirExists,
+  checkOpts,
+  generateCodeFile,
+};
 
-module.exports = {transformUrlStr}
+/**
+ * @typedef {Object} DefaultOpts
+ * @property {boolean} [singleFile] - If true, only a single file will be processed.
+ * @property {boolean} [dir] - If true, files in the directory will be processed.
+ * @property {boolean} [recursive] - If true, the files will be processed recursively.
+ */
+
+/**
+ * @param {DefaultOpts} opts
+ * @returns {DefaultOpts}
+ */
+function checkOpts(opts) {
+  //null
+  if (typeof opts === "object" && !opts) {
+    const defaultOpts = {
+      singleFile: true,
+      dir: false,
+      recursive: false,
+    };
+
+    return defaultOpts;
+  }
+  if ((opts.singleFile && opts.dir) || (opts.singleFile && opts.recursive)) {
+    throw new Error(
+      `Options dir ${opts.dir} or recursive ${opts.recursive} cannot be set alongside singleFile ${opts.singleFile}`
+    );
+  }
+
+  return opts;
+}
 /**
  *
  * @param {string | URL} url
  */
 function transformUrlStr(url) {
-    if (typeof url !== "string" && !(url instanceof URL)) {
-      throw new TypeError(
-        `Param must be of type string or URL; Received object: ${url} of type ${typeof url}`
-      );
-    }
-  
-    if (typeof url === "string") {
-      url = new URL(url);
-    }
-  
-    if (url.hostname !== "github.com") {
-      throw new Error(
-        `Only support repos from github.com; Received hostname '${url.hostname}'`
-      );
-    }
-  
-    const urlPathname = url.pathname;
-  
-    const parts = urlPathname.split("/").filter(Boolean);
-  
-    const owner = parts[0];
-    const repoName = parts[1];
-    const repoPath = parts.slice(4).join("/");
-  
-    const apiUrl = `https://api.github.com/repos/${owner}/${repoName}/contents/${repoPath}`;
-  
-    return apiUrl;
+  if (typeof url !== "string" && !(url instanceof URL)) {
+    throw new TypeError(
+      `Param must be of type string or URL; Received object: ${url} of type ${typeof url}`
+    );
   }
+
+  if (typeof url === "string") {
+    url = new URL(url);
+  }
+
+  if (url.hostname !== "github.com") {
+    throw new Error(
+      `Only support repos from github.com; Received hostname '${url.hostname}'`
+    );
+  }
+
+  const urlPathname = url.pathname;
+
+  const parts = urlPathname.split("/").filter(Boolean);
+
+  const owner = parts[0];
+  const repoName = parts[1];
+  const repoPath = parts.slice(4).join("/");
+
+  const CONTENTS_API_URL = `https://api.github.com/repos/${owner}/${repoName}/contents/${repoPath}`;
+
+  return CONTENTS_API_URL;
+}
+
+/**
+ *  Create dir if it does NOT exist; Exit with 1 if error
+ * @param {string} dirPath
+ */
+function ensureDirExists(dirPath) {
+  try {
+    if (!fs.existsSync(dirPath)) {
+      fs.mkdirSync(dirPath);
+    }
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+function generateCodeFile(dirPath, filename, content) {
+  try {
+    fs.writeFileSync(path.join(dirPath, filename), content, {
+      encoding: "utf-8",
+    });
+
+    console.log("Code Files written!");
+  } catch (err) {
+    console.error(`Error writing file ${o.name}: ${err}`);
+    // process.exit(1);
+    throw err;
+  }
+}
